7ee87ed47af129f8bfe0ae3a7b7b145714397175
Assaf Paz <damagedspline@gmail.com>
2011-11-23 23:37:41 +0200

    Deploy day/night schemas on first run

--- waze/qt/deploy_data.sh
+++ waze/qt/deploy_data.sh
@@ -17,6 +17,9 @@ echo Preparing Waze map directory in ${MAP_TARGET_DIR}
 mkdir -p ${MAP_TARGET_DIR}
 cp -r ${SOURCE_DIR}/maps ${MAP_TARGET_DIR} 2> /dev/null
 
+echo Deploying day/night schema directories to ${WAZE_TARGET_DIR}/skins/default
 mkdir -p ${WAZE_TARGET_DIR}/skins/default
+cp -r ${SOURCE_DIR}/skins/default/day ${WAZE_TARGET_DIR}/skins/default 2> /dev/null
+cp -r ${SOURCE_DIR}/skins/default/night ${WAZE_TARGET_DIR}/skins/default 2> /dev/null
 
 touch /home/user/.waze/data_deployed_0.0.2
