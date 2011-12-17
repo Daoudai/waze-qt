055e3ffa9059a37a497f7d8e36f25ac0e01dc304
Assaf Paz <damagedspline@gmail.com>
2011-12-17 20:34:41 +0200

    Fix the issue where last input text remained on next open + now the dialogs has the same bg color as most of the app

--- waze/qt/qt_main.cc
+++ waze/qt/qt_main.cc
@@ -48,6 +48,7 @@
 
 extern "C" {
 #include "roadmap_lang.h"
+#include "roadmap_skin.h"
 #ifdef PLAY_CLICK
 #include "roadmap_sound.h"
 #endif
@@ -238,6 +239,7 @@ void RMapMainWindow::addCanvas(void) {
 }
 
 void RMapMainWindow::showContactList() {
+    QObject *item;
     if (contactsDialog == NULL) {
         contactsDialog = new QDeclarativeView(this);
         contactsDialog->setGeometry(0, 0, canvas->width(), canvas->height());
@@ -248,16 +250,8 @@ void RMapMainWindow::showContactList() {
         contactsDialog->setSource(QUrl::fromLocalFile("/opt/waze/qml/Contacts.qml"));
 #endif
         contactsDialog->setAttribute(Qt::WA_TranslucentBackground);
-        QObject *item = dynamic_cast<QObject*>(contactsDialog->rootObject());
-        item->setProperty("width", canvas->width());
-        item->setProperty("height", canvas->height());
-        if (roadmap_lang_rtl())
-        {
-            item->setProperty("isRtl", QVariant(true));
-        }
-        item->setProperty("okButtonText", QString::fromLocal8Bit(roadmap_lang_get("Ok")));
-        item->setProperty("cancelButtonText", QString::fromLocal8Bit(roadmap_lang_get("Back_key")));
-        item->setProperty("title", QString::fromLocal8Bit(roadmap_lang_get("Contacts")));
+        item = dynamic_cast<QObject*>(contactsDialog->rootObject());
+
         QObject::connect(item, SIGNAL(okPressed(QString)),
                          this, SLOT(contactsDialogOkPressed(QString)));
         QObject::connect(item, SIGNAL(cancelPressed()),
@@ -265,6 +259,21 @@ void RMapMainWindow::showContactList() {
         QObject::connect(item, SIGNAL(mouseAreaPressed()),
                          this, SLOT(mouseAreaPressed()));
     }
+    else
+    {
+        item = dynamic_cast<QObject*>(contactsDialog->rootObject());
+    }
+
+    item->setProperty("width", canvas->width());
+    item->setProperty("height", canvas->height());
+    item->setProperty("color", roadmap_skin_state()? "#74859b" : "#70bfea"); // ssd_container::draw_bg()
+    if (roadmap_lang_rtl())
+    {
+        item->setProperty("isRtl", QVariant(true));
+    }
+    item->setProperty("okButtonText", QString::fromLocal8Bit(roadmap_lang_get("Ok")));
+    item->setProperty("cancelButtonText", QString::fromLocal8Bit(roadmap_lang_get("Back_key")));
+    item->setProperty("title", QString::fromLocal8Bit(roadmap_lang_get("Contacts")));
 
     mouseAreaPressed();
     contactsDialog->show();
