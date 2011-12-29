3fa0c9a7743720a23309277d005ff24689727cac
asaf <asaf@asaf-desktop>
2011-12-29 19:12:33 +0200

    Merge the following to v2.4.0: generic_search.c update_range.c RealtimeUsers.c roadmap_address_ssd.c roadmap_address_tc.c roadmap_res.c roadmap_search.c

--- waze/Realtime/RealtimeUsers.c
+++ waze/Realtime/RealtimeUsers.c
@@ -531,7 +531,7 @@ static BOOL post_comment_keyboard_callback(int         exit_code,
 }
 
 static void disclaimer_cb( int exit_code ){
-#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE_NATIVE) || defined(ANDROID))
+#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE_NATIVE) || defined(ANDROID) || defined(QTMOBILITY)
     ShowEditbox(roadmap_lang_get("Chit chat"), "", post_comment_keyboard_callback,
           &g_user, EEditBoxEmptyForbidden | EEditBoxAlphaNumeric );
 #else
@@ -560,7 +560,7 @@ static int ping (LPRTUserLocation user){
       return 0;
    }
 
-#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE_NATIVE) || defined(ANDROID))
+#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE_NATIVE) || defined(ANDROID)) || defined(QTMOBILITY)
     ShowEditbox(roadmap_lang_get("Chit chat"), "", post_comment_keyboard_callback,
           &g_user, EEditBoxEmptyForbidden | EEditBoxAlphaNumeric );
 #else
--- waze/address_search/generic_search.c
+++ waze/address_search/generic_search.c
@@ -309,7 +309,7 @@ BOOL on_favorites_name( int         exit_code,
 void generic_search_add_to_favorites()
 {
 
-#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE) || defined(ANDROID) )
+#if ((defined(__SYMBIAN32__) && !defined(TOUCH_SCREEN)) || defined(IPHONE) || defined(ANDROID) ) || defined(QTMOBILITY)
    ShowEditbox(roadmap_lang_get("Name"), "",
             on_favorites_name, NULL, EEditBoxStandard | EEditBoxAlphaNumeric );
 #else
