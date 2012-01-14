03e8ad95440d031d25f184d58ff3158f5759f770
damagedspline <damagedspline@gmail.com>
2012-01-14 09:28:12 +0200

    Small fixes to the DB layer of tts & map tiles

--- waze/qt/roadmap_tile_storage_qtsql.cc
+++ waze/qt/roadmap_tile_storage_qtsql.cc
@@ -120,15 +120,17 @@ static BOOL check_sqlite_error_line( const char* errstr, bool code, int line )
 	{
             QByteArray errorBA;
             const char* errmsg = "";
+            int errorCode = 0;
             if (sgSQLiteDb)
             {
                 errorBA = sgSQLiteDb->lastError().text().toLocal8Bit();
                 errmsg = errorBA.data();
+                errorCode = sgSQLiteDb->lastError().number();
             }
 
             errstr = errstr ? errstr : "";
             roadmap_log( ROADMAP_MESSAGE_ERROR, __FILE__, line, "SQLite error in %s executing sqlite statement. Error : %d ( %s )",
-                            errstr, code, errmsg );
+                            errstr, errorCode, errmsg );
             return FALSE;
 	}
 	return TRUE;
