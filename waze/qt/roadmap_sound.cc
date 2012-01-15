700dfb0e2d79c8ea34f0113216867791aec5db0d
apaz <apaz@2T6DB4J-451.adprod.bmc.com>
2012-01-15 15:10:49 +0200

    Align the roadmap_sound.cc with Android's roadmap_sound.c

--- waze/qt/roadmap_sound.cc
+++ waze/qt/roadmap_sound.cc
@@ -46,14 +46,6 @@ extern "C" {
 #include "roadmap_prompts.h"
 }
 
-const int SND_VOLUME_LVLS_COUNT = 4;
-const int SND_VOLUME_LVLS[] = {0, 1, 2, 3};
-const char* SND_VOLUME_LVLS_LABELS[SND_VOLUME_LVLS_COUNT];
-const char* SND_DEFAULT_VOLUME_LVL = "2";
-
-static RoadMapConfigDescriptor RoadMapConfigVolControl =
-                        ROADMAP_CONFIG_ITEM( "Voice", "Volume Control" );
-
 typedef struct roadmap_sound_st {
     QMediaResource *media;
 } roadmap_sound_st;
@@ -63,27 +55,71 @@ extern RMapMainWindow* mainWindow;
 Playlist *mediaPlayer = NULL;
 Recorder *recorder = NULL;
 
-RoadMapSoundList roadmap_sound_list_create (int flags) {
+static RoadMapConfigDescriptor RoadMapConfigVolControl =
+                        ROADMAP_CONFIG_ITEM( "Voice", "Volume Control" );
 
+const int SND_VOLUME_LVLS_COUNT = 4;
+const int SND_VOLUME_LVLS[] = {0, 1, 2, 3};
+const char* SND_VOLUME_LVLS_LABELS[SND_VOLUME_LVLS_COUNT];
+const char* SND_DEFAULT_VOLUME_LVL = "2";
+
+static const char* get_full_name( const char* name );
+
+void roadmap_sound_initialize ()
+{
+        // Initialize the volume labels for GUI
+        SND_VOLUME_LVLS_LABELS[0] = roadmap_lang_get( "Silent" );
+        SND_VOLUME_LVLS_LABELS[1] = roadmap_lang_get( "Low" );
+        SND_VOLUME_LVLS_LABELS[2] = roadmap_lang_get( "Medium" );
+        SND_VOLUME_LVLS_LABELS[3] = roadmap_lang_get( "High" );
+
+        // Set current volume from the configuration
+        roadmap_config_declare("user", &RoadMapConfigVolControl, SND_DEFAULT_VOLUME_LVL, NULL );
+
+        mediaPlayer = new Playlist(mainWindow);
+        recorder = new Recorder(mainWindow);
+
+        roadmap_sound_set_volume(roadmap_config_get_integer(&RoadMapConfigVolControl));
+}
+
+void roadmap_sound_shutdown   ()
+{
+    delete mediaPlayer;
+    delete recorder;
+}
+
+RoadMapSoundList roadmap_sound_list_create (int flags)
+{
    RoadMapSoundList list =
             (RoadMapSoundList) calloc (1, sizeof(struct roadmap_sound_list_t));
+
    list->flags = flags;
 
    return list;
 }
 
+int roadmap_sound_list_add (RoadMapSoundList list, const char *name)
+{
+  const char* full_name;
+  if ( list->count == MAX_SOUND_LIST ) return SND_LIST_ERR_LIST_FULL;
 
-int roadmap_sound_list_add (RoadMapSoundList list, const char *name) {
+  full_name = get_full_name( name );
 
-   if (list->count == MAX_SOUND_LIST) return -1;
+  if ( !roadmap_file_exists( full_name, NULL ) )
+  {
+     roadmap_log( ROADMAP_ERROR, "File %s doesn't exist! Cannot add to the list.", full_name );
+     return SND_LIST_ERR_NO_FILE;
+  }
 
-   strncpy_safe (list->list[list->count], name, sizeof(list->list[0]));
-   list->count++;
+  strncpy (list->list[list->count], name, sizeof(list->list[0]));
+  list->list[list->count][sizeof(list->list[0])-1] = '\0';
+  list->count++;
 
-   return list->count - 1;
+  return list->count - 1;
 }
 
-int roadmap_sound_list_add_buf (RoadMapSoundList list, void* buf, size_t size ) {
+int roadmap_sound_list_add_buf (RoadMapSoundList list, void* buf, size_t size )
+{
     char path[512];
     int file_num = list->count;
     RoadMapFile file;
@@ -116,13 +152,89 @@ int roadmap_sound_list_add_buf (RoadMapSoundList list, void* buf, size_t size ) 
     return list->count - 1;
 }
 
+int roadmap_sound_play_list (const RoadMapSoundList list)
+{
+    if (mediaPlayer != NULL)
+    {
+		int listSize = roadmap_sound_list_count( list );
+		const char *full_name;
+		int i;
+
+        for ( i = 0; i < roadmap_sound_list_count(list); i++) 
+		{
+            const char *name = roadmap_sound_list_get ( list, i );
+
+			if ( (list->flags & SOUND_LIST_BUFFERS) == 0 )
+             {
+                 full_name = get_full_name( name );
+                 // Calling the JNI layer
+                 mediaPlayer->playMedia(QUrl::fromLocalFile(full_name));
+             }
+             else
+             {
+                /*
+                 * Temporary solution - write the buffer to the file for further playing
+                 * AGA
+                 */
+    //            FreeMapNativeSoundManager_PlayFile( roadmap_sound_list_get ( list, i ) );
+             //			   FreeMapNativeSoundManager_PlayBuffer( list->buf_list[i], list->buf_list_sizes[i] );
+    //            free( list->buf_list[i] );
+             }
+        }
+    }
+	// Deallocation
+    if ( (list->flags & SOUND_LIST_NO_FREE) == 0x0 )
+	{
+        roadmap_sound_list_free  (list);
+    }
+    return 0;
+}
+
+
+
+
+RoadMapSound roadmap_sound_load (const char *path, const char *file, int *mem)
+{
+
+   char *full_name = roadmap_path_join (path, file);
+   const char *seq;
+   RoadMapFileContext sound;
+   char sound_filename[MAX_SOUND_NAME];
+
+   return NULL;
+
+   snprintf( sound_filename, sizeof(sound_filename), "%s.mp3", full_name);
+
+   seq = roadmap_file_map (NULL, sound_filename, NULL, "r", &sound);
+
+   roadmap_path_free (full_name);
+
+   if (seq == NULL) {
+      *mem = 0;
+      return NULL;
+   }
+
+   *mem = roadmap_file_size (sound);
+
+   return (RoadMapSound) sound;
+}
+
+int roadmap_sound_free (RoadMapSound sound)
+{
+
+    delete sound->media;
+    delete sound;
+
+   return 0;
+}
+
 int roadmap_sound_list_count (const RoadMapSoundList list) {
 
    return list->count;
 }
 
-
-const char *roadmap_sound_list_get (const RoadMapSoundList list, int i) {
+const char *roadmap_sound_list_get (const RoadMapSoundList list, int i)
+{
 
    if (i >= MAX_SOUND_LIST) return NULL;
 
@@ -133,26 +245,6 @@ const char *roadmap_sound_list_get (const RoadMapSoundList list, int i) {
 void roadmap_sound_list_free (RoadMapSoundList list) {
 
    free(list);
-}
-
-
-RoadMapSound roadmap_sound_load (const char *path, const char *file, int *mem) {
-    RoadMapSound sound = new roadmap_sound_st;
-    QString fullPath = QString(path).append("/").append(file);
-    sound->media = new QMediaResource(QUrl::fromLocalFile(fullPath));
-
-    *mem = sound->media->dataSize();
-
-    return sound;
-}
-
-
-int roadmap_sound_free (RoadMapSound sound) {
-
-    delete sound->media;
-    delete sound;
-
-   return 0;
 }
 
 
@@ -169,46 +261,6 @@ int roadmap_sound_play_file (const char *file_name) {
 }
 
 
-int roadmap_sound_play_list (const RoadMapSoundList list) {
-
-    if (mediaPlayer != NULL)
-    {
-        QString path(roadmap_path_downloads());
-        path.append("/").append("sound").append("/").append(roadmap_prompts_get_name()).append("/");
-
-        for (int i = 0; i < list->count; i++) {
-            QString full_path(path);
-            QString sound_path;
-            if (QFile::exists(list->list[i]))
-            {
-                sound_path = QString(list->list[i]);
-            }
-            else if (QFile::exists(full_path.append(list->list[i]).append(".mp3")))
-            {
-                sound_path = QString(full_path);
-                roadmap_log(ROADMAP_WARNING, "No path provided assuming as <%s>", full_path.toLocal8Bit().data());
-            }
-            else
-            {
-               roadmap_log(ROADMAP_ERROR, "Sound file not exist <%s>", list->list[i]);
-               continue;
-            }
-
-            mediaPlayer->playMedia(QUrl::fromLocalFile(sound_path));
-        }
-    }
-    else
-    {
-        roadmap_log(ROADMAP_WARNING, "Requested to play audio, but player not initialized yet...");
-    }
-
-    if (!(list->flags & SOUND_LIST_NO_FREE)) {
-        roadmap_sound_list_free  (list);
-    }
-    return 0;
-}
-
-
 int roadmap_sound_record (const char *file_name, int seconds) {
     recorder->recordMedia(QUrl::fromLocalFile(file_name), seconds*1000);
    return 0;
@@ -216,28 +268,6 @@ int roadmap_sound_record (const char *file_name, int seconds) {
 
 void roadmap_sound_stop_recording (void) {
     recorder->stop();
-}
-
-void roadmap_sound_initialize (void)
-{
-        // Initialize the volume labels for GUI
-        SND_VOLUME_LVLS_LABELS[0] = roadmap_lang_get( "Silent" );
-        SND_VOLUME_LVLS_LABELS[1] = roadmap_lang_get( "Low" );
-        SND_VOLUME_LVLS_LABELS[2] = roadmap_lang_get( "Medium" );
-        SND_VOLUME_LVLS_LABELS[3] = roadmap_lang_get( "High" );
-
-        // Set current volume from the configuration
-        roadmap_config_declare("user", &RoadMapConfigVolControl, SND_DEFAULT_VOLUME_LVL, NULL );
-
-        mediaPlayer = new Playlist(mainWindow);
-        recorder = new Recorder(mainWindow);
-
-        roadmap_sound_set_volume(roadmap_config_get_integer(&RoadMapConfigVolControl));
-}
-
-void roadmap_sound_shutdown   (void) {
-    delete mediaPlayer;
-    delete recorder;
 }
 
 /***********************************************************
@@ -254,4 +284,28 @@ void roadmap_sound_set_volume ( int volLvl )
 
     // Log the operation
     roadmap_log( ROADMAP_DEBUG, "Current volume is set to level : %d.", volLvl );
+
+}
+
+
+static const char* get_full_name( const char* name )
+{
+   static char full_name[256];
+   const char *suffix = "";
+
+   if ( !strchr( name, '.' ) )
+   {
+      suffix = ".mp3";
+   }
+
+   if ( roadmap_path_is_full_path( name ) )
+   {
+      strncpy_safe( full_name, name, sizeof( full_name ) );
+   }
+   else
+   {
+      snprintf( full_name, sizeof( full_name ), "%s//%s//%s//%s%s",
+            roadmap_path_downloads(), "sound", roadmap_prompts_get_name(), name, suffix );
+   }
+   return full_name;
 }
