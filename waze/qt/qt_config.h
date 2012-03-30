3eda2bb403a74aaf61ea77b411109c20a4b97f74
damagedspline <damagedspline@gmail.com>
2012-03-30 10:08:34 +0300

    Make RMapWazeConfig standalone class, concentrating the impl behind it. Fix RMapWazeConfig dtor.

--- waze/qt/qt_config.h
+++ waze/qt/qt_config.h
@@ -4,26 +4,44 @@
 #include <QObject>
 #include <QSettings>
 #include <QHash>
-#include "qt_main.h"
+#include <QString>
+#include "qt_global.h"
 
 extern "C" {
 #include "roadmap_config.h"
 }
 
+struct RoadMapConfigItemRecord {
+    QString file;
+    QVariant default_value;
+    WazeString strValue;
+    unsigned char type;
+
+    const char* name;
+    const char* category;
+
+    RoadMapCallback callback;
+
+    QList<WazeString> enumeration_values;
+    QList<WazeString>::const_iterator enum_iter;
+
+    QHash<QString, RoadMapConfigItem* >::const_iterator items_iter;
+};
+
 class RMapConfig : public QObject
 {
     Q_OBJECT
 public:
-    typedef QHash<QString, RoadMapConfigItem*> ItemsHash;
+    typedef QHash<QString, struct RoadMapConfigItemRecord* > ItemsHash;
 
-    explicit RMapConfig(RMapMainWindow *parent);
+    explicit RMapConfig(QObject *parent);
     virtual ~RMapConfig();
 
     void saveAllSettings();
-    QSettings* getSettings(const QString file);
+    QSettings* getSettings(QString file);
 
-    void addConfigItem(QString file, QString name, RoadMapConfigItem* item);
-    RoadMapConfigItemRecord* getConfigItem(QString file, QString name);
+    void addConfigItem(QString file, QString name, struct RoadMapConfigItemRecord* item);
+    struct RoadMapConfigItemRecord* getConfigItem(QString file, QString name);
 
     ItemsHash::const_iterator getItemsConstBegin(QString file);
     ItemsHash::const_iterator getItemsConstEnd(QString file);
@@ -32,6 +50,6 @@ private:
 
     QHash<QString, QSettings*> _settings;
     QHash<QString, ItemsHash> _configItems;
-}
+};
 
 #endif // QT_RCONFIG_H
