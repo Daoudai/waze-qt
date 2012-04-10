a1f118df83bb2b363d82ea9677f6cfc6e065c4b7
damagedspline <damagedspline@gmail.com>
2012-04-10 23:47:20 +0300

    rewrite the network layer from semi scratch with Waze(TM) foundations

--- waze/qt/qt_network.h
+++ waze/qt/qt_network.h
@@ -26,76 +26,38 @@ class RNetworkSocket : public QObject {
     Q_OBJECT
 
 public:
-    RNetworkSocket(RNetworkManager* parent, QNetworkRequest* request, RequestType type, void *context);
+    RNetworkSocket(QAbstractSocket* socket, bool isCompressed);
 
     virtual ~RNetworkSocket();
 
-    void setCallback(RoadMapInput callback);
+    void setCallback(RoadMapInput callback, RoadMapIO* io);
 
     void commitRequest(QByteArray data = QByteArray());
 
     void waitUntilFinished();
 
+    bool connectSocket(QUrl& url);
+
+    int socketDescriptor();
+
     int read(char* data, int size);
 
-    int write(char* data, int size);
+    int write(char* data, int size, bool immediate = true);
 
     void abort();
 
-public slots:
-    void invokeCallback();
-
 private slots:
-    void finished();
-
-    void onCallbackChanged();
-
-    void timerEvent(QTimerEvent *te);
+    void executeCallback();
 
 signals:
-    void timedout();
-    void finished(RNetworkSocket* socket);
     void callbackChanged();
-    void callbackExecuted();
 
 private:
     RoadMapInput _callback;
-
-    RoadMapHttpCompCtx _compressContext;
     bool _isCompressed;
-    QNetworkRequest* _request;
-    QNetworkReply* _reply;
+    QAbstractSocket* _socket;
+    roadmap_http_comp_t* _compressContext;
     RoadMapIO _io;
-    int _timerId;
-    QSemaphore _pendingFinish;
-    RNetworkManager* _networkManager;
-    RequestType _type;
-};
-
-class RNetworkManager : public QNetworkAccessManager
-{
-    Q_OBJECT
-public:
-    explicit RNetworkManager(QObject *parent = 0);
-
-    virtual ~RNetworkManager();
-
-    RNetworkSocket* requestSync(RequestType protocol, QUrl url,
-                     QDateTime update_time,
-                     int flags,
-                     roadmap_result* err);
-
-    RNetworkSocket* requestAsync(RequestType protocol, QUrl url,
-                      QDateTime update_time,
-                      int flags,
-                      RoadMapNetConnectCallback callback,
-                      void *context);
-
-private:
-    void prepareNetworkRequest(QNetworkRequest& req,
-                               QUrl url,
-                               QDateTime update_time,
-                               int flags);
 };
 
 #endif // QT_NETWORK_H
