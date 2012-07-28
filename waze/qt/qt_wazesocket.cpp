b3aa758ad8fd8944148d98ca8043d1d94b60deb7
damagedspline <damagedspline@gmail.com>
2012-07-28 16:57:18 +0300

    Finalizing the override, ignore HTTP header as QHttp cant deal with it properly after the ack removal. Also fix the Waze response tags not parsed correctly.

--- waze/qt/qt_wazesocket.cpp
+++ waze/qt/qt_wazesocket.cpp
@@ -10,18 +10,41 @@ qint64 WazeSocket::readData(char * data, qint64 maxSize)
 {
     static const QString ACK = QString::fromAscii("ack\r\n");
     static const int ACK_LENGTH = ACK.length();
+    static const QString HTTP = QString::fromAscii("HTTP/1.");
+    static const int HTTP_LENGTH = HTTP.length();
+    static const QString DATA_DELIMITER = QString::fromAscii("\r\n\r\n");
+    static const int DATA_DELIMITER_LENGTH = DATA_DELIMITER.length();
+
+    qMemSet(data, 0, maxSize);
 
     qint64 length = QSslSocket::readData(data, maxSize);
     if (_firstRead)
     {
-
-        if (length == ACK_LENGTH && qstrnicmp(ACK.toAscii().constData(), data, ACK_LENGTH) == 0)
+        if (length >= ACK_LENGTH && qstrnicmp(ACK.toAscii().constData(), data, ACK_LENGTH) == 0)
         {
-            length = QSslSocket::readData(data, maxSize);
+            if (data[ACK_LENGTH] == 0)
+            {
+                length = QSslSocket::readData(data, maxSize);
+            }
+
+            if (data[ACK_LENGTH] > 0)
+            {
+                length -= ACK_LENGTH;
+                qMemCopy(data, data + ACK_LENGTH, length);
+            }
         }
 
         _firstRead = false;
     }
+    else if (length >= HTTP_LENGTH && qstrnicmp(HTTP.toAscii().constData(), data, HTTP_LENGTH) == 0)
+    {
+        QByteArray array(data, length);
+        int index = array.indexOf(DATA_DELIMITER) + DATA_DELIMITER_LENGTH;
+        array.remove(0, index);
+        length = array.length();
+        qMemCopy(data, array.constData(), length);
+    }
+
 
     return length;
 }
